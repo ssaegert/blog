485d04a112aa011053ef8b5ec77c72cf1d29d79a
ssaegert <ssaegert@gmail.com>
2018-04-18 14:26:41 -0500

    Completed Views and Services exercises

--- src/main/java/com/example/blog/Controllers/PostController.java
+++ src/main/java/com/example/blog/Controllers/PostController.java
@@ -1,34 +1,83 @@
 package com.example.blog.Controllers;
 
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+//
+//@Controller
+//public class PostController {
+//    @GetMapping("/posts")
+//    @ResponseBody
+//    public String allPosts() {
+//        return "posts index page";
+//    }
+//
+//    @GetMapping("/posts/{id}")
+//    @ResponseBody
+//    public String singlePost(@PathVariable int id) {
+//        return "viewing post number - " + id;
+//    }
+//
+//    @GetMapping("/posts/create")
+//    @ResponseBody
+//    public String createPostForm() {
+//        return "view the form for creating a post";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    @ResponseBody
+//    public String createPost() {
+//        return "create a new post";
+//    }
+//}
+import com.example.blog.Post;
+import com.example.blog.services.PostService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
+
+    private final PostService postsvc;
+
+
+    public PostController(PostService posts) {
+        this.postsvc = posts;
+    }
+
+
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String allPosts() {
-        return "posts index page";
+    public String index(Model model) {
+        model.addAttribute("posts", postsvc.getAllPosts());
+        return "/posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String singlePost(@PathVariable int id) {
-        return "viewing post number - " + id;
+    public String show(@PathVariable long id, Model model) {
+        model.addAttribute("posts", postsvc.getPost(id));
+        return "/posts/show";
     }
 
     @GetMapping("/posts/create")
     @ResponseBody
-    public String createPostForm() {
-        return "view the form for creating a post";
+    public String create() {
+        return "Here is the post create form...";
     }
 
     @PostMapping("/posts/create")
     @ResponseBody
-    public String createPost() {
-        return "create a new post";
+    public String insert() {
+        return "Inserted new post!";
     }
+
 }
---
+++ src/main/java/com/example/blog/services/PostService.java
@@ -0,0 +1,50 @@
+package com.example.blog.services;
+
+import com.example.blog.Post;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PostService {
+    private List<Post> posts;
+
+    public PostService() {
+        this.posts = new ArrayList<>();
+        createPost();
+    }
+
+    public List<Post> getAllPosts() {
+        return posts;
+    }
+
+    public Post getPost(long id) {
+        return posts.get((int) id - 1);
+    }
+
+
+    //    public PostService () {
+//        posts = new ArrayList<>();
+//        createPost();
+//    }
+
+//    public List<Post> findAll() {
+//        posts = new ArrayList<>();
+//        createPost();
+//        return posts;
+//    }
+
+//    public List<Post> findPostByTitle(String title) {
+//
+//        int index = posts.indexOf(title);
+//    }
+
+
+    private void createPost() {
+        posts.add(new Post("New Post Title 1", "This is a description of post 1."));
+        posts.add(new Post("New Post Title 2", "This is a description of post 2."));
+        posts.add(new Post("New Post Title 3", "This is a description of post 3."));
+        posts.add(new Post("New Post Title 4", "This is a description of post 4."));
+    }
+}
